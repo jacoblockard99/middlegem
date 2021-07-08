2508c61a63264bdaecbb2723c363487f917d228b
Jacob <jacoblockard99@gmail.com>
2021-07-07 20:24:47 -0400

    Document Middlegem
    Write YARD docs in 'middlegem.rb' for the Middlegem module and the
    Middlegem::Error class.

--- lib/middlegem.rb
+++ lib/middlegem.rb
@@ -2,7 +2,20 @@
 
 require_relative 'middlegem/version'
 
+# The namespace containing all modules in the middlegem gem.
+#
+# @author Jacob Lockard
+# @since 0.1.0
 module Middlegem
+  # A subclass of {StandardError} from which all custom errors in the middlegem gem are derived.
+  #
+  # One potential reason to use this class is for rescuing all custom errors produced by
+  # middlegem. For example:
+  #   begin
+  #     # Do something risky with middlegem here...
+  #   rescue Middlegem::Error
+  #     # Catch any middlegem-specific error here...
+  #   end
+  # @see https://ruby-doc.org/core-2.0.0/Exception.html
   class Error < StandardError; end
-  # Your code goes here...
 end
